80a6406dd295210313a90cea55a398aadc26ea53
Will Spurgeon <wspurgeon@wayfair.com>
2017-11-02 13:01:39 -0400

    Fixed drawing from delegate methods. mousePressed now works but the super's method must also be called.

--- Processing/ProcessingView.swift
+++ Processing/ProcessingView.swift
@@ -32,28 +32,44 @@ open class ProcessingView: NSView, UserProgram {
         Enviroment.mouseX = Double(Float(theEvent.locationInWindow.x))
         Enviroment.mouseY = Double(Float(theEvent.locationInWindow.y))
         self.mouseMoved()
+        updateViews(timer: self.redrawTimer)
     }
-    
+
     override open func mouseEntered(with theEvent: NSEvent) {
+        self.redrawTimer.invalidate()
         Enviroment.mouseIsInView = true
         Enviroment.currentCursor.set()
+        updateViews(timer: self.redrawTimer)
+        startTimer()
     }
     
     override open func mouseExited(with theEvent: NSEvent) {
+        self.redrawTimer.invalidate()
         Enviroment.mouseIsInView = false
         defaultCursor.set()
+        updateViews(timer: self.redrawTimer)
+        startTimer()
     }
     
     override open func mouseDragged(with theEvent: NSEvent) {
+        self.redrawTimer.invalidate()
         self.mouseDragged()
+        updateViews(timer: self.redrawTimer)
+        startTimer()
     }
     
     override open func mouseUp(with theEvent: NSEvent) {
+        self.redrawTimer.invalidate()
         self.mouseReleased()
+        updateViews(timer: self.redrawTimer)
+        startTimer()
     }
     
     override open func mouseDown(with theEvent: NSEvent) {
+        self.redrawTimer.invalidate()
         self.mousePressed()
+        updateViews(timer: self.redrawTimer)
+        startTimer()
     }
     
     func commonSetup() {
@@ -81,15 +97,22 @@ open class ProcessingView: NSView, UserProgram {
             
             drawingView.setNeedsDisplay(NSRect(x: 0, y: 0, width: Enviroment.w, height: Enviroment.h))
         } else {
-            Enviroment.listOfDrawOps = []
-            self.draw()
-            Enviroment.frameCount += 1
+            if self.redrawTimer.isValid {
+                Enviroment.listOfDrawOps = []
+                self.draw()
+                Enviroment.frameCount += 1
+            }
+
             if !drawableArraysAreEqual(Enviroment.listOfDrawOps, oldDrawQueue) && !Enviroment.listOfDrawOps.isEmpty {
                 let image = self.image()
                 self.backingView.image = image
                 drawingView.setNeedsDisplay(NSRect(x: 0, y: 0, width: Enviroment.w, height: Enviroment.h))
             }
         }
+    }
+
+    func startTimer() {
+        redrawTimer = Timer.scheduledTimer(withTimeInterval: Enviroment.frameTime, repeats: true, block: updateViews)
     }
 }
 
--- User Program/MainProgram.swift
+++ User Program/MainProgram.swift
@@ -9,92 +9,21 @@
 import Processing
 
 class MainProgram: ProcessingView {
-    var gameStart = true
-    
-    var x = 150.0
-    var y = 150.0
-    var speedX = random(lowerLimit: 5, upperLimit: 10)
-    var speedY = random(lowerLimit: 5, upperLimit: 10)
-    var leftColor = 128
-    var rightColor = 128
-    var diam = 20
-    var rectSize = 150
-    var diamHit = 0.0
-    
+
     override func setup() {
         size(width: 500, height: 500)
         noStroke()
-        fill(r: 0, g: 0, b: 0)
-        background(r: 123, g: 30, b: 240)
-        frameRate(fps: 30)
+        background(r: 0, g: 0, b: 255)
     }
 
-    var i = 0
-    var alias = true
     override func draw() {
-        background(r: 123, g: 30, b: 240)
-        antiAliasing(on: alias)
-        ellipse(x: 200, y: 200, w: 200, h: 200)
-        alias = !alias
+        fill(r: mouseX, g: mouseX/2, b: 255 - mouseX)
+        ellipse(x: Int(mouseX), y: Int(mouseY), w: 25, h: 25)
     }
-    
-//    override func setup() {
-//        size(width: 250, height: 250)
-//        frameRate(fps: 60)
-//        noStroke()
-//    }
-//
-//    override func draw() {
-//        background(r: 255, g: 255, b: 255)
-//
-//        ellipse(x: Int(x), y: Int(y), w: diam, h: diam)
-//
-//        fill(r: Double(leftColor), g: Double(leftColor), b: Double(leftColor))
-//        rectangle(x: 0, y: 0, w: 20, h: height)
-//        fill(r: Double(rightColor), g: Double(rightColor), b: Double(rightColor))
-//        rectangle(x: width-30, y: Int(mouseY - Double(rectSize/2)), w: 10, h: rectSize)
-//
-//        if gameStart {
-//
-//            x = x + speedX
-//            y = y + speedY
-//
-//            // if ball hits movable bar, invert X direction and apply effects
-//            if x > Double(width - 30) && x < Double(width - 20) && y > mouseY - Double(rectSize/2) && y < mouseY + Double(rectSize/2) {
-//                speedX = speedX * -1
-//                x = x + speedX
-//                rightColor = 0
-//                diamHit = random(lowerLimit: 75, upperLimit: 150)
-//                rectSize = rectSize - 10
-//                rectSize = constrain(rectSize, low: 10,high: 150)
-//            }
-//                // if ball hits wall, change direction of X
-//            else if (x < 25) {
-//                speedX = speedX * -1.1
-//                x = x + speedX
-//                leftColor = 0
-//            } else {
-//                leftColor = 128
-//                rightColor = 128
-//            }
-//            // resets things if you lose
-//            if (x > Double(width)) {
-//                gameStart = false
-//                x = 150
-//                y = 150
-//                speedX = random(lowerLimit: 3, upperLimit: 5)
-//                speedY = random(lowerLimit: 3, upperLimit: 5)
-//                rectSize = 150
-//            }
-//            // if ball hits up or down, change direction of Y
-//            if ( y > Double(height) || y < 0 ) {
-//                speedY = speedY * -1
-//                y = y + speedY
-//            }
-//        }
-//    }
-//
-//    override func mouseDown(with theEvent: NSEvent) {
-//        gameStart = !gameStart
-//    }
+
+    override open func mouseDown(with theEvent: NSEvent) {
+        background(r: 0, g: 0, b: 255)
+        ellipse(x: Int(mouseX), y: Int(mouseY), w: 25, h: 25)
+        super.mouseDown(with: theEvent)
+    }
 }
